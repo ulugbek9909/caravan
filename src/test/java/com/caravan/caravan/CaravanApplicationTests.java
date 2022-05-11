ae6cd4ee99023108c36188e0474df6e77196d814
ulugbek9909 <ulugbekkhan94@mail.ru>
2022-05-11 15:10:41 +0500

    git

---
+++ src/test/java/com/caravan/caravan/CaravanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caravan.caravan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaravanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
