b42be7a91bc0164b4def38452f7d8c4309d55c53
Akobir <akobir.abduganiev@ya.ru>
2022-05-12 17:01:30 +0500

    GuideProfileEntity.java added

--- src/main/java/com/caravan/caravan/entity/GuideProfileEntity.java
+++ src/main/java/com/caravan/caravan/entity/GuideProfileEntity.java
@@ -1,15 +1,27 @@
 package com.caravan.caravan.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
 @Table(name = "guide_profile")
 public class GuideProfileEntity extends BaseEntity {
-    @Column
+    @Column(unique = true)
     private String phoneNumber;
     @Column(columnDefinition = "text")
     private String content;
+    @Column
+    private Boolean isHiring;
+    @Column
+    private Double guideRate;
 
+    @Column(name = "profile_id")
+    private String profileId;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "profile_id", insertable = false, updatable = false)
+    ProfileEntity profile;
 }
