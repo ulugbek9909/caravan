17f20210c684df8ba1d31e64414e14128d50f3ee
Sarvarbek <mukhtarovsarvarbek@gmail.com>
2022-05-12 16:42:55 +0500

    BaseEntity qoshildi

--- src/main/java/com/caravan/entity/BaseEntity.java
+++ src/main/java/com/caravan/entity/BaseEntity.java
@@ -1,21 +1,28 @@
 package com.caravan.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
 import javax.persistence.*;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @MappedSuperclass
+@Getter
+@Setter
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id", nullable = false)
     private UUID id;
 
-    public UUID getId() {
-        return id;
-    }
 
-    public void setId(UUID id) {
-        this.id = id;
-    }
+    @Column
+    @CreationTimestamp
+    private LocalDateTime createdDate;
+
+
+
 
 }
