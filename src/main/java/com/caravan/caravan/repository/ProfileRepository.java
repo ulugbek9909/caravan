6c17c34d723b35228b3c5fa4ec3d9d568811c206
Sarvarbek <mukhtarovsarvarbek@gmail.com>
2022-05-12 17:20:05 +0500

    ProfileService,repository qoshildi. hali yoziladi

---
+++ src/main/java/com/caravan/caravan/repository/ProfileRepository.java
@@ -0,0 +1,11 @@
+package com.caravan.caravan.repository;
+
+import com.caravan.caravan.entity.ProfileEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ProfileRepository extends JpaRepository<ProfileEntity, UUID> {
+    Optional<ProfileEntity> findByPhoneNumber(String phoneNumber);
+}
--- src/main/java/com/caravan/caravan/service/ProfileService.java
+++ src/main/java/com/caravan/caravan/service/ProfileService.java
@@ -1,11 +1,20 @@
 package com.caravan.caravan.service;
 
+import com.caravan.caravan.dto.ProfileDTO;
+import com.caravan.caravan.repository.ProfileRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProfileService {
+    private final ProfileRepository repository;
 
+
+    public ProfileDTO create(ProfileDTO dto){
+        repository.findByPhoneNumber(dto.getPhoneNumber());
+        return  null;
+    }
 }
