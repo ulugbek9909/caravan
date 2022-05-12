fc6b54b9ede6347588b4d5db69a488585e05e0da
Sarvarbek <mukhtarovsarvarbek@gmail.com>
2022-05-12 16:54:47 +0500

    Profile entity boldi

--- src/main/java/com/caravan/caravan/entity/ProfileEntity.java
+++ src/main/java/com/caravan/caravan/entity/ProfileEntity.java
@@ -1,10 +1,32 @@
 package com.caravan.caravan.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import com.caravan.caravan.enums.ProfileGender;
+import com.caravan.caravan.enums.ProfileRole;
+import com.caravan.caravan.enums.ProfileStatus;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
 
 @Entity
 @Table(name = "profile_entity")
+@Getter
+@Setter
 public class ProfileEntity extends BaseEntity {
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String surname;
+    @Column
+    private String email;
+    @Column
+    @Enumerated(EnumType.STRING)
+    private ProfileRole role;
+    @Column
+    @Enumerated(EnumType.STRING)
+    private ProfileStatus status;
+    @Column
+    @Enumerated(EnumType.STRING)
+    private ProfileGender gender;
 
 }
