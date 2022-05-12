4d9eb73c8fdc21110405dba403beca846c735c90
ulugbek9909 <ulugbekkhan94@mail.ru>
2022-05-12 16:48:59 +0500

    git

--- src/main/java/com/caravan/entity/TripEntity.java
+++ src/main/java/com/caravan/entity/TripEntity.java
@@ -1,4 +1,16 @@
 package com.caravan.entity;
 
-public class TripEntity {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class TripEntity extends BaseEntity{
+
+
 }
---
+++ src/main/java/com/caravan/service/TripRepository.java
@@ -0,0 +1,9 @@
+package com.caravan.service;
+
+import com.caravan.entity.TripEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface TripRepository extends JpaRepository<TripEntity, UUID> {
+}
