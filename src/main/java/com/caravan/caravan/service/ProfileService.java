41eff03c38f825e0f86cf35c858727fee2b9120b
Sarvarbek <mukhtarovsarvarbek@gmail.com>
2022-05-12 16:57:03 +0500

    profileService ochildi

---
+++ src/main/java/com/caravan/caravan/service/ProfileService.java
@@ -0,0 +1,11 @@
+package com.caravan.caravan.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileService {
+
+}
