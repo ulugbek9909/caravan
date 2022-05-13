c74f5b8a30280fdc87756c1f87a342b8cbf2062c
ulugbek9909 <ulugbekkhan94@mail.ru>
2022-05-13 16:30:53 +0500

    Refresh

---
+++ src/main/java/com/caravan/caravan/repository/FacilityRepository.java
@@ -0,0 +1,7 @@
+package com.caravan.caravan.repository;
+
+import com.caravan.caravan.entity.FacilityEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FacilityRepository extends JpaRepository<FacilityEntity, Long> {
+}
---
+++ src/main/java/com/caravan/caravan/repository/GuideRateRepository.java
@@ -0,0 +1,7 @@
+package com.caravan.caravan.repository;
+
+import com.caravan.caravan.entity.GuideRateEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuideRateRepository extends JpaRepository<GuideRateEntity, Long> {
+}
